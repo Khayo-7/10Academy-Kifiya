7ba69d55d3bbceb5bb41007670108f1c1c904ded
Khalid Mohammed <khayo7moh@gmail.com>
2024-12-08 14:13:44 -0300

    Added statistical analysis and basic cleaning (filling fr missing data and dropping duplicates) for the benin-malanville.csv dataset

--- app/utils.py
+++ app/utils.py
@@ -4,3 +4,4 @@ def extract_datasets(input_data_path, output_data_path):
 
     with zipfile.ZipFile(input_data_path, 'r') as zip:    
         zip.extractall(output_data_path)
+        print(f"Extracted {input_data_path} to {output_data_path}")
